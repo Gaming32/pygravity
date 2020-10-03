ac20a9a40a05f9e02d6baa7508c7723add2a33f3
Gaming32 <soujournme@gmail.com>
2020-10-03 12:05:51 -0500

    1.0.0 released! (pre-documentation)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = 'python-gravity',
-    version = '0.5.3',
+    version = '1.0.0b1',
     url = 'https://github.com/gaming32/pygravity',
     author = 'Gaming32',
     author_email = 'gaming32i64@gmail.com',
