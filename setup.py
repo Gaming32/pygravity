bf7e5e0251ac997a9387e50174268cbce7efb546
Gaming32 <soujournme@gmail.com>
2021-03-11 11:37:56 -0600

    add much documentation to PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as fp:
 
 setuptools.setup(
     name = 'Python-Gravity',
-    version = '1.0.0.2',
+    version = '1.0.1',
     url = 'https://github.com/gaming32/pygravity',
     author = 'Gaming32',
     author_email = 'gaming32i64@gmail.com',
